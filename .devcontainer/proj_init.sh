d866cb30dd355a2deddb979c7c63a5bf7a082534
witekgeb <witekgeb@gmail.com>
2023-05-19 13:34:31 +0000

    Pending changes exported from your codespace

---
+++ .devcontainer/proj_init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd /workspaces
+git clone -b 1.4.0 https://github.com/raspberrypi/pico-sdk.git
+cd pico-sdk
+git submodule update --init
+
+cd /workspaces 
+git clone -b V10.5.0 https://github.com/FreeRTOS/FreeRTOS-Kernel.git
+
+cd Anjay-pico-client-training
+git submodule update --init --recursive 
+mkdir build
