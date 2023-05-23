16a9b4f7e16d9ff1b442b8433d019950fadca6ba
witekgeb <witekgeb@gmail.com>
2023-05-23 10:56:06 +0200

    Update proj_init.sh

--- .devcontainer/proj_init.sh
+++ .devcontainer/proj_init.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
 cd /workspaces
-git clone -b 1.4.0 https://github.com/raspberrypi/pico-sdk.git
-cd pico-sdk
-git submodule update --init
-
-cd /workspaces 
-git clone -b V10.5.0 https://github.com/FreeRTOS/FreeRTOS-Kernel.git
-
+git clone -b 1.4.0 --recursive https://github.com/raspberrypi/pico-sdk.git
+git clone -b V10.5.0 --recursive  https://github.com/FreeRTOS/FreeRTOS-Kernel.git
 cd Anjay-pico-client-training
-git submodule update --init --recursive 
-mkdir build
+git submodule update --init --recursive
